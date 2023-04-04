74ab57f3062ba83c7d0cbe756948f2b42bbd1764
RyanGomeZz <ryangomes1103@gmail.com>
2023-04-04 08:30:45 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        {
+            // declaração de variaveis
+            int numero1 = 0;
+            int numero2 = 0;
+
+            //entrada de dados
+            Scanner obj;
+            obj = new Scanner(System.in);
+            System.out.println("Entre com o número");
+            numero1 = Integer.parseInt(obj.nextLine());
+            //processamento
+            //saida de dados
+        }
+}
+}
